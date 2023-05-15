8af15e7858b11c86e7827a0d3b6819287f4c278c
nehlinshanila <nehlinshanila@gmail.com>
2023-05-15 11:40:06 +0600

    deleted bubblesort.py

--- Test/Bubble_sort.py
+++
@@ -1,6 +0,0 @@
-def bubble_sort(array):
-    array_length = len(array)
-    for i in range(array_length - 1):
-        for j in range(array_length - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
